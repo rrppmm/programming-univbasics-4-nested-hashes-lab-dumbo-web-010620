43a40c0819390d9ba315d7809fba2bbae3ea7db8
Paul M <paulmcadory@gmail.com>
2019-12-31 21:03:40 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,13 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +51,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -66,6 +66,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LSIP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -101,7 +105,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     programmer_hash
 end
